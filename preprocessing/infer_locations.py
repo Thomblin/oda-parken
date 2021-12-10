9fa4b8fdb71a656676140a0e16dffd1d74366d61
Tom Hülsmann <tominator96@live.de>
2021-12-10 20:59:13 +0100

    significantly improved parser, recognizes ~80 of the locations with an exact address (street and number).

--- preprocessing/infer_locations.py
+++ preprocessing/infer_locations.py
@@ -13,16 +13,14 @@ for year in years[1:]:
 fines = fines.rename(columns={"Tatort 2": "location_text"})
 fines.location_text = fines.location_text.astype(str)
 
-#exploration
-#print([i for i in list(fines["location_text"].str.contains("bla")) if i != False])
-
-#print(len(fines))
-#print(len(fines[fines["location_text"].str.contains("Haus-Nr.")]))
-    
-#print(fines.head())
-
 def parse_street_no(number):
-
+    
+    #handle street nos with letters in them by removing them (future potential for improvement!)
+    number = "".join([char for char in number if char.isdigit()])
+    
+    if len(number) == 0:
+        return
+    
     if "-" in number:
         try:
             return [int(no) for no in number.split("-")]
@@ -35,7 +33,7 @@ def parse_street_no(number):
             return None
     else:
         try:
-            return [int(components[1])]
+            return [int(number)]
         except ValueError:
             return None
     
@@ -44,47 +42,67 @@ locations = list(fines["location_text"])
 
 identified = 0
 for location in locations:
+    was_identified = False
     #bring input into a uniform format and split into components
     components = location.lower().split()
     
-    #sometimes the oprators write the streetname in two words
-    #special exception for streets starting with "am" or "im"
-    if len(components) > 1 and components[1] in ["straße", "gässchen", "weg", "gasse"] or components[0] == "im" or components[0] == "am":
-        components = ["".join([components[0], components[1]])] + components[2:]
-    
-    #check locations of the form: "streetname streetnumber"
-    if len(components) == 1:
-        if ("straße" in components[0]) or ("weg" in components[0]) or ("gasse" in components[0]) or ("gässchen" in components[0]):
-            streetname = components[0]
-            identified += 1
-            
-    elif len(components) == 2:
-        street_no = parse_street_no(components[1])
+    major_part = []
+    relation = ""
+    minor_part = []
 
-        if street_no != None:
-            streetname = components[0] #CAKE remove ,
-            identified += 1
-    elif len(components) == 4:
-        if ("ggü." in components or "gegenüber" in components) and "haus-nr." in components:
-            street_no = parse_street_no(components[3])
-            if street_no != None:
-                streetname = components[0] #CAKE remove ,
-                identified += 1
-            continue
-        elif "neben" in components and "haus-nr." in components:
-            street_no = parse_street_no(components[3])
-            if street_no != None:
-                streetname = components[0] #CAKE remove ,
-                identified += 1
-            continue
-        elif "vor" in components and "haus-nr." in components:
-            street_no = parse_street_no(components[3])
-            if street_no != None:
-                streetname = components[0] #CAKE remove ,
-                identified += 1
-            continue
+    for i in range(0, len(components)):
+        component = components[i]
+        if component in ["ggü.", "vor", "neben", "ecke", "gegenüber", "haltestelle", "laterne", "hinter"]:
+            if component == "ggü." or component == "gegenüber":
+                relation = "ggü."
+            elif component == "neben" or component == "hinter":
+                relation = "vor"
+            else:
+                relation = component
+                
+            minor_part = components[i+1:]
+            break
+        elif parse_street_no(component) != None:
+            relation = "number"
+            minor_part = component
+            break
+        else:
+            major_part.append(component)
+            
+    street_name = ""
+    street_no = 0
+            
+    if relation == "number":
+        street_name = " ".join(major_part)
+        street_no = parse_street_no(minor_part)
+        was_identified = True
+    
+    elif relation == "vor":
+        if "haus-nr." in minor_part and parse_street_no(minor_part[-1]):
+            street_name = " ".join(major_part)
+            street_no = parse_street_no(minor_part[-1])
+            was_identified = True
+            
+    elif relation == "ggü.":
+        if "haus-nr." in minor_part and parse_street_no(minor_part[-1]):
+            street_name = " ".join(major_part)
+            street_no = parse_street_no(minor_part[-1])
+            was_identified = True
+    
+    # street crossings are disabled for now, but are an opportnunity for improvement in the future
+    #elif relation == "ecke":
+    #    if len(minor_part) == 1:
+    #        street_name = " ".join(major_part)
+    #        minor_street_name = minor_part[0]
+    #        was_identified = True
+            
+    
+    if was_identified:
+        identified += 1
+        #print(street_name + " " + str(street_no))
     else:
-        print(components)
+        print(str(major_part) + "/" + relation + "/" + str(minor_part))
+      
         
-#performance: 0.449
+#performance: 0.8083
 print(identified / len(locations))
